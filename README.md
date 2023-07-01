311b09087f94b259a9e1f13bb0bb625ea7b4db1a
Yann Klein <yann.klein@me.com>
2023-07-01 09:21:02 +0900

    created restaurant model

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## Building steps
 1. Create rails app with `rails new appname`
+2. Generate the model `rails g model Restaurant name address rating:integer`
+3. `rails db:migrate`
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_01_001308) do
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "rating", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
